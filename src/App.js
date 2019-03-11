ead625a3c16420483b2c5150b88b4cdc5e967345
Wafi <septya.wafi@gmail.com>
2019-03-11 13:30:13 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -3,23 +3,66 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      active: {
+        name: 'Home',
+        color: 'red',
+        gambar: 'red.png'
+      },
+    };
+  }
+
+  clicked(menu){
+    this.setState({
+      active: menu,
+    });
+  }
+  
+  showImage(){
+    console.log(this.state.active.gambar);
+    return <img className="image" src={this.state.active.gambar}></img>
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div id="app">
+        {/* map akan loop sebanyak menu yang didefinisikan */}
+        {/* kemudian mengembalikan elemen <a/> */}
+        <nav className="nav">{this.props.items.map((menu, index) => {
+          var style = 'menu';
+
+          if (this.state.active.name === menu.name) {
+            style = `${style} ${menu.color} is-activate`;
+            
+            console.log(menu.name);
+            console.log(this.state.active.img);
+          }
+          return(
+            <a className={style}
+            // bind untuk membuat 'menu' bisa dikirim ke fungsi 'clicked'
+            onClick={
+              this.clicked.bind(this, menu)
+            }
+            key={index}
+            >
+              {menu.name}
+            </a>
+          );
+        })}
+        </nav>
+
+        <div className="info">
+        
+          ini adalah <span className={this.state.active.color + " selected"}>
+            {this.state.active.name}
+          </span>
+        </div>
+        {this.showImage()}
+        
+        
       </div>
     );
   }
