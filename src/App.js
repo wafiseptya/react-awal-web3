f7767baaeb4238e66b19c3a3c42daf88f2de877e
Wafi <septya.wafi@gmail.com>
2019-04-10 22:25:25 +0700

    kalkulator

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ class App extends Component {
     super(props);
 
     this.state = {
+      a: null,
+      b: null,
+      hasil: null,
+      operator: null,
       active: {
         name: 'Home',
         color: 'red',
@@ -21,9 +25,58 @@ class App extends Component {
     });
   }
   
+
+  calculate(e){
+    e.preventDefault();
+
+    const a = parseInt(this.refs.a.value);
+    const b = parseInt(this.refs.b.value);
+    const operator = this.refs.operator.value;
+
+    var hasil;
+    switch (operator) {
+      case '+':
+        hasil = a+b;
+        break;
+    
+      case '-':
+        hasil = a-b;
+        break;
+    
+      case '*':
+        hasil = a*b;
+        break;
+    
+      case '/':
+        hasil = a/b;
+        break;
+    }
+
+    this.setState({ a, b, hasil, operator});
+
+  }
+
   showImage(){
     console.log(this.state.active.gambar);
     return <img className="image" src={this.state.active.gambar}></img>
+  }
+  
+  kalkulator(){
+
+    const {hasil, a, b, operator} = this.state;
+
+    if(this.state.hasil) {
+      return (
+        <div>
+          <br/>
+          <p className="alert alert-success">
+            {a+operator+b+' = '+hasil}
+          
+          </p>
+        </div>
+      );
+    }
+    
   }
 
   render() {
@@ -61,6 +114,42 @@ class App extends Component {
           </span>
         </div>
         {this.showImage()}
+        
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-md-6">
+            <form onSubmit={this.calculate.bind(this)}>
+            <div className="row">
+              <div className="col-md-5 p-0">
+                <div className="input-group">
+                  <input type="number" ref="a" className="form-control" placeholder="input 1"></input>
+                </div>
+              </div>
+
+              <div className="col-md-2 p-0">
+                <div className="input-group">
+                  <select type="number" ref="operator" className="form-control" placeholder="input 1">
+                    <option value="+">+</option>
+                    <option value="-">+</option>
+                    <option value="*">x</option>
+                    <option value="/">/</option>
+                  </select>
+                </div>
+              </div>
+
+              <div className="col-md-5 p-0">
+                <div className="input-group">
+                  <input type="number" ref="b" className="form-control" placeholder="input 2"></input>
+                </div>
+              </div>
+              <button className="btn btn-primary" onKeyPress={this.kalkulator()}>Pencet Gan</button>
+            </div>
+            </form>
+              {this.kalkulator()}
+            </div>
+          </div>
+        </div>
+
         
         
       </div>
